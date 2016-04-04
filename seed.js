12635fa6aedb8cfc9619f26c0e366e115150c401
mradambeck <mr.adambeck@gmail.com>
2016-04-03 21:31:32 -0700

    changes to seed file to work for heroku

--- seed.js
+++ seed.js
@@ -2,51 +2,51 @@
 // simply run: `node seed.js` from the root of this project folder.
 
 var db = require('./models');
-//
-// var person_seed = [
-//   {
-//     name: "Adam Beck",
-//     github_link: "https://github.com/mradambeck",
-//     github_profile_image: "https://avatars.githubusercontent.com/u/8527273?v=3",
-//     current_city: "Oakland, CA",
-//     pets: ["Baxter", "Simon"]
-//   },
-//   {
-//     name: "Sherah Smith",
-//     github_link: "https://github.com/sherah",
-//     github_profile_image: "https://avatars.githubusercontent.com/u/1241135?v=3",
-//     current_city: "Oakland, CA",
-//     pets: []
-//   }
-// ];
-//
-// var artist_seed = [
-//   {
-//     name: "Deth P. Sun",
-//     instagram_link: "https://www.instagram.com/dethpsun/",
-//     facebook_link: "https://www.facebook.com/dethpsun/",
-//     website_link: "http://dethpsun.com/",
-//   },
-//   {
-//     name: "David Huffman",
-//     website_link: "http://david-huffman.com/"
-//   },
-//   {
-//     name: "Marci Washington",
-//     instagram_link: "https://www.instagram.com/marciwash/",
-//     facebook_link: "https://www.facebook.com/Marci-Washington-118217581571034/",
-//     website_link: "http://www.marciwashington.com/",
-//   },
-//   {
-//     name: "Mary Snowden",
-//     facebook_link: "https://www.facebook.com/mary.snowden.18"
-//   },
-//   {
-//     name: "Michael Beck",
-//     facebook_link: "https://www.facebook.com/michael.beck.988373",
-//     website_link: "http://www.michaelbeck.com/"
-//   }
-// ];
+
+var person_seed = [
+  {
+    name: "Adam Beck",
+    github_link: "https://github.com/mradambeck",
+    github_profile_image: "https://avatars.githubusercontent.com/u/8527273?v=3",
+    current_city: "Oakland, CA",
+    pets: ["Baxter", "Simon"]
+  }
+  // {
+  //   name: "Sherah Smith",
+  //   github_link: "https://github.com/sherah",
+  //   github_profile_image: "https://avatars.githubusercontent.com/u/1241135?v=3",
+  //   current_city: "Oakland, CA",
+  //   pets: []
+  // }
+];
+
+var artist_seed = [
+  {
+    name: "Deth P. Sun",
+    instagram_link: "https://www.instagram.com/dethpsun/",
+    facebook_link: "https://www.facebook.com/dethpsun/",
+    website_link: "http://dethpsun.com/",
+  },
+  {
+    name: "David Huffman",
+    website_link: "http://david-huffman.com/"
+  },
+  {
+    name: "Marci Washington",
+    instagram_link: "https://www.instagram.com/marciwash/",
+    facebook_link: "https://www.facebook.com/Marci-Washington-118217581571034/",
+    website_link: "http://www.marciwashington.com/",
+  },
+  {
+    name: "Mary Snowden",
+    facebook_link: "https://www.facebook.com/mary.snowden.18"
+  },
+  {
+    name: "Michael Beck",
+    facebook_link: "https://www.facebook.com/michael.beck.988373",
+    website_link: "http://www.michaelbeck.com/"
+  }
+];
 
 
 
